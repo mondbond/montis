aba4a98a30aaf3fc6740873dd0a1e4fb8cbd1dfd
ivan_bagrii <ivanbagrii7@gmail.com>
2020-04-04 13:32:06 +0300

    move service to com package

--- montis/src/main/java/com/service/FileMetadataValidator.java
+++ montis/src/main/java/com/service/FileMetadataValidator.java
@@ -1,4 +1,4 @@
-package service;
+package com.service;
 
 import org.springframework.stereotype.Service;
 
--- montis/src/main/java/com/service/storage/FileStorage.java
+++ montis/src/main/java/com/service/storage/FileStorage.java
@@ -1,4 +1,4 @@
-package service.storage;
+package com.service.storage;
 
 import org.springframework.web.multipart.MultipartFile;
 
